7d87a97a5847680c7bf27bb497123c74b4aab2cb
Jared R. White <jaredwhite128@gmail.com>
2022-03-18 00:39:40 -0400

    Make the dockerfile path relative to the launcher.

--- jetson/launch-interactive-container.sh
+++ jetson/launch-interactive-container.sh
@@ -5,7 +5,7 @@ set -xe
 DIR="$(dirname "$0")"
 DIR="$(cd "$DIR" && pwd)"
 
-docker build -t robot .
+docker build -t robot "$DIR"
 exec docker run -it --rm \
 	-p 8080:8080 \
 	-p 5540-5549:5540-5549 \
