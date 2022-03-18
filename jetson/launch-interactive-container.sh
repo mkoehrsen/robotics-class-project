374aa32975099a26cbb22d0109b7b90e22af2772
Jared R. White <jaredwhite128@gmail.com>
2022-03-17 21:56:18 -0400

    Add dockerfile and a launcher script that passes baseline args.

---
+++ jetson/launch-interactive-container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xe
+
+DIR="$(dirname "$0")"
+DIR="$(cd "$DIR" && pwd)"
+
+docker build -t robot .
+exec docker run -it --rm \
+	-p 8080:8080 \
+	-p 5540-5549:5540-5549 \
+	-v /tmp/argus_socket:/tmp/argus_socket \
+	-v "$PWD":/mnt/host \
+	robot
